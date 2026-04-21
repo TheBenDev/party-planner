9dad96d6d3de01d64828fa859a6fd2ea1adb8061
Benjamin Bruington <Psychological_Chemist@hotmail.com>
2026-04-20 23:25:05 -0400

    add a global error boundry to the wep application

--- apps/web/src/router.tsx
+++ apps/web/src/router.tsx
@@ -1,10 +1,14 @@
 import { createRouter as createTanStackRouter } from "@tanstack/react-router";
 import { getContext } from "@/integrations/tanstack-query/root-provider";
+import { RootErrorBoundary } from "./components/error-boundary";
+import { RootNotFound } from "./components/not-found";
 import { routeTree } from "./routeTree.gen";
 
 export function getRouter() {
 	const router = createTanStackRouter({
 		context: getContext(),
+		defaultErrorComponent: RootErrorBoundary,
+		defaultNotFoundComponent: RootNotFound,
 		defaultPreload: "intent",
 		defaultPreloadStaleTime: 0,
 		routeTree,
