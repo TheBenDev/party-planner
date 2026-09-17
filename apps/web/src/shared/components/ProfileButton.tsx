2db9e1540d17670d0c0efbdf7f2660beb5c8016c
Benjamin Bruington <Psychological_Chemist@hotmail.com>
2026-09-17 13:29:25 -0500

    redesign profile button

--- apps/web/src/shared/components/ProfileButton.tsx
+++ apps/web/src/shared/components/ProfileButton.tsx
@@ -1,11 +1,14 @@
 import { useClerk } from "@clerk/clerk-react";
 import { Link } from "@tanstack/react-router";
+import { Castle, ChevronDown, Moon, Settings, Sun } from "lucide-react";
 import { toast } from "sonner";
 import { useAuth } from "@/shared/hooks/auth";
 import { client } from "@/shared/lib/client";
-import ThemeSwitch from "./ThemeSwitch";
+import { useTheme } from "../hooks/theme";
+import { Button } from "./ui/button";
 import {
 	DropdownMenu,
+	DropdownMenuCheckboxItem,
 	DropdownMenuContent,
 	DropdownMenuItem,
 	DropdownMenuLabel,
@@ -17,6 +20,7 @@ import { Skeleton } from "./ui/skeleton";
 
 export default function ProfileButton() {
 	const { signOut } = useClerk();
+	const { theme, toggleTheme } = useTheme();
 	const { campaign: campaignAuth, user: userAuth, userIsLoading } = useAuth();
 	const user = userAuth?.user;
 	const campaign = campaignAuth?.campaign;
@@ -42,7 +46,7 @@ export default function ProfileButton() {
 				<DropdownMenuTrigger asChild>
 					<button
 						aria-label="Open profile menu"
-						className="hover:cursor-pointer mr-3"
+						className="group flex items-center gap-1 cursor-pointer mr-3 focus:outline-none"
 						type="button"
 					>
 						{user.avatar ? (
@@ -54,17 +58,20 @@ export default function ProfileButton() {
 								width={30}
 							/>
 						) : (
-							<span>{user.firstName ?? "Profile"}</span>
+							<span>
+								{user.firstName ?? "Profile"} {user.lastName}
+							</span>
 						)}
+						<ChevronDown className="h-4 w-4 text-muted-foreground transition-transform duration-200 group-data-[state=open]:rotate-180" />
 					</button>
 				</DropdownMenuTrigger>
-				<DropdownMenuContent className="flex flex-col p-0">
+				<DropdownMenuContent className="flex flex-col p-0 min-w-48">
 					{user.email && (
 						<DropdownMenuLabel className="w-full text-start border-b-[1px] bg-accent text-xs">
-							{user.email.length > 15 ? (
+							{user.email.length > 26 ? (
 								<HoverCard>
 									<HoverCardTrigger>
-										{`${user.email.substring(0, 15)}...`}
+										{`${user.email.substring(0, 25)}...`}
 									</HoverCardTrigger>
 									<HoverCardContent className="flex items-center justify-center h-5 w-auto py-0 text-xs text-center bg-accent">
 										{user.email}
@@ -75,32 +82,51 @@ export default function ProfileButton() {
 							)}
 						</DropdownMenuLabel>
 					)}
-					<DropdownMenuItem className="w-full">
+					<DropdownMenuCheckboxItem
+						checked={theme === "dark"}
+						className="w-full text-lg"
+						onCheckedChange={toggleTheme}
+						onSelect={(e) => e.preventDefault()}
+					>
+						{theme === "dark" ? (
+							<Sun className="h-3 w-3 text-primary" />
+						) : (
+							<Moon className="h-3 w-3 text-primary" />
+						)}
+						Dark Mode
+					</DropdownMenuCheckboxItem>
+					<DropdownMenuItem className="w-full text-lg">
+						<Castle className="h-3 w-3 text-primary" />
 						{campaign ? (
-							<Link className="w-full text-left text-sm" to="/campaign">
+							<Link className="w-full text-left" to="/campaign">
 								{campaign.title}
 							</Link>
 						) : (
-							<Link className="w-full text-left text-sm" to="/campaign/create">
+							<Link className="w-full text-left" to="/campaign/create">
 								Create Campaign
 							</Link>
 						)}
 					</DropdownMenuItem>
-					<DropdownMenuItem className="w-full">
+					<DropdownMenuItem className="w-full text-lg">
+						<Settings className="h-3 w-3 text-primary" />
 						<Link
-							className="w-full text-left text-sm flex items-center gap-2"
+							className="w-full text-left flex items-center gap-2"
 							to="/settings"
 						>
-							Settings
+							Profile Settings
 						</Link>
 					</DropdownMenuItem>
 					<DropdownMenuSeparator />
-					<DropdownMenuItem className="w-full text-sm" onClick={handleSignOut}>
-						Sign Out
+					<DropdownMenuItem
+						className="w-full bg-accent"
+						onClick={handleSignOut}
+					>
+						<Button className="w-full text-xl bg-accent text-primary hover:bg-accent">
+							Sign Out
+						</Button>
 					</DropdownMenuItem>
 				</DropdownMenuContent>
 			</DropdownMenu>
-			<ThemeSwitch id="theme" />
 		</div>
 	);
 }
